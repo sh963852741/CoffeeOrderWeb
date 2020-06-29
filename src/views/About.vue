3e56e2aa94a70f73227cfd43e81936e120c20475
sh963852741 <sh963852741@126.com>
2020-06-29 10:05:56 +0800

    演示页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <Row>
+        <span>{{inputValue}}</span>
+        <Input :value="inputValue"/>
+        <Button @click="getUserList">点我</Button>
+    </Row>
 </template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            inputValue: "123"
+        };
+    },
+    mounted() {
+        // setTimeout(() => {
+        //     this.inputValue = "456";
+        // }, 1000);
+    },
+    methods: {
+        getUserList() {
+            this.inputValue = "456";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
