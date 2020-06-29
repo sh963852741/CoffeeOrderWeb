95da3790f5633e144cba170e0a4432ca0541e442
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-06-29 16:21:22 +0800

    登录界面

--- src/views/LogIn/LogIn.vue
+++ src/views/LogIn/LogIn.vue
@@ -1,17 +1,22 @@
 <template>
     <Row type="flex" justify="center">
         <i-col span="6">
-            <Form :model="logInModel">
-                <FormItem prop="userName" label="用户名">
-                    <i-input v-model="logInModel.userName"></i-input>
-                </FormItem>
-                <FormItem prop="password" label="密码">
-                    <i-input v-model="logInModel.password" placeholder="请输入您的密码" type="password"></i-input>
-                </FormItem>
-                <FormItem>
-                    <Button type="primary" @click="logIn">登录</Button>
-                </FormItem>
-            </Form>
+            <Row>
+                用户登录
+            </Row>
+            <Row>
+                <Form :model="logInModel">
+                    <FormItem prop="userName" label="用户名">
+                        <i-input v-model="logInModel.userName"></i-input>
+                    </FormItem>
+                    <FormItem prop="password" label="密码">
+                        <i-input v-model="logInModel.password" placeholder="请输入您的密码" type="password"></i-input>
+                    </FormItem>
+                    <FormItem>
+                        <Button type="primary" @click="logIn">登录</Button>
+                    </FormItem>
+                </Form>
+            </Row>
         </i-col>
     </Row>
 </template>
@@ -25,22 +30,10 @@ export default {
             logInModel: {}
         };
     },
-    mounted() {
-        this.getUserList();
-    },
+    mounted() {},
     methods: {
-        getUserList() {
-            axios.get('/CoffeeTest/demo1', {})
-            .then(response => {
-                this.userList = response.data;
-                console.log(response);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
         logIn() {
-            axios.post("XXX", {userName: this.logInModel.userName, password: this.logInModel.password})
+            axios.post("/CoffeeTest/api/usermanage/login", {userName: this.logInModel.userName, password: this.logInModel.password})
             .then(response => {
                 console.log(response);
             })
--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,11 @@ module.exports = {
     publicPath: '/',
     devServer: {
         proxy: {
-            '/CoffeeTest': {
-                target: 'http://106.13.201.225:8080', //API服务器的地址
+            '/api': {
+                target: 'http://106.13.201.225:8080/CoffeeTest/api/', //API服务器的地址
                 changeOrigin: true
             }
         }
+
     }
 }
