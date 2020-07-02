c7189746df9cbb9b6547341e75afce7bdfd32c07
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-07-02 21:35:07 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8088/CoffeeOrderService/', //API服务器的地址
-                /* http://106.13.201.225:8080/CoffeeOrderService/ */
+                target: 'http://106.13.201.225:8080/CoffeeOrderService/', //API服务器的地址
+                /* http://localhost:8088/CoffeeOrderService/ */
                 changeOrigin: true
             }
         }
