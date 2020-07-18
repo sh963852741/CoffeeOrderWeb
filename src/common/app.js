5e8a4b45925dd64d10c4b7bfb53d5834b07aeadc
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-07-18 21:42:41 +0800

    后台自动登录

--- src/common/app.js
+++ src/common/app.js
@@ -1,4 +1,13 @@
 let appSetting = {
-
+    loginSettings: {
+        rememberPwd: false,
+        username: "",
+        password: ""
+    }
+}
+/* 查找是否记住密码 */
+let loginSettings = JSON.parse(localStorage.getItem("loginSettings"));
+if(loginSettings){
+    appSetting.loginSettings = loginSettings;
 }
 export default appSetting;
