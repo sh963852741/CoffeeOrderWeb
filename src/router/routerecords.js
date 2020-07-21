02bda1bbb73e15d0a5d260e965174cd09d0884b9
18gy888 <49858931+18gy888@users.noreply.github.com>
2020-07-21 09:29:51 +0800

    合并master

--- src/router/routerecords.js
+++ src/router/routerecords.js
@@ -58,6 +58,20 @@ const routes = [
       meta: {layout: "admin"},
       component: () => import(/* webpackChunkName: "users" */ '@/views/UserManager/UserDetail.vue')
     },
+    //个人中心
+    {
+        path: '/usermanager/usercenter',
+        name: 'UserCenter',
+        meta: {layout: "admin"},
+        component: () => import(/* webpackChunkName: "users" */ '@/views/UserManager/UserCenter.vue')
+      },
+      //前台个人中心
+      {
+        path: '/usermenu/usercenter',
+        name: 'UserMenuCenter',
+        meta: {layout: "account"},
+        component: () => import(/* webpackChunkName: "users" */ '@/views/UserManager/UserCenter.vue')
+      },
     //前台用户订餐页面
     {
       path: '/usermenu/ordermenu',
